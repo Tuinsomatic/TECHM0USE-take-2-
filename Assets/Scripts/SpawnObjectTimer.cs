18ea31e9bac80a6e7331e3681be3b979295a8968
Malleoluses R <33025211+Tuinsomatic@users.noreply.github.com>
2018-05-24 13:53:24 +0100

    Assets (2) and .vs

---
+++ Assets/Scripts/SpawnObjectTimer.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnObjectTimer : MonoBehaviour {
+
+    public float spawnTime = 5.0f;
+
+	void Start () {
+        Invoke("DoSpawn", spawnTime);
+	}
+
+    void DoSpawn()
+    {
+        SendMessage("Spawn");
+        Invoke("DoSpawn", spawnTime);
+        if (spawnTime == 1)
+        {
+            spawnTime = 1;
+        }
+        else
+        {
+            spawnTime -= 0.01f;
+        }
+    }
+}
---
+++ Assets/Scripts/ZombieBehaviour.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class ZombieBehaviour : MonoBehaviour {
+
+    public int health = 10;
+    public int damage = 2;
+    public GameObject explosionPrefab;
+    public float adjustExplosionAngle = 0.0f;
+
+    public Transform player;
+
+    void Start()
+    {
+        if (GameObject.FindWithTag("Player"))
+        {
+            player = GameObject.FindWithTag("Player").transform;
+
+            GetComponent<MoveTowards>().target = player;
+            GetComponent<LookAtPlayer>().target = player;
+        }
+    }
+
+    void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            other.gameObject.SendMessage("TakeDamage", damage);
+        }
+    }
+
+    public void TakeDamage(int damage)
+    {
+        health -= damage;
+
+        if(health <= 0)
+        {
+            Quaternion newRot = Quaternion.Euler(transform.eulerAngles.x,
+                                                 transform.eulerAngles.y,
+                                                 transform.eulerAngles.z + adjustExplosionAngle);
+
+            Instantiate(explosionPrefab, transform.position, newRot);
+
+            GetComponent<AddScore>().DoSendScore();
+            Destroy(gameObject);
+        }
+    }
+
+    void FixedUpdate()
+    {
+        {
+            GetComponent<Rigidbody2D>().velocity = Vector2.zero;
+            GetComponent<Rigidbody2D>().angularVelocity = 0.0f;
+        }
+    }
+}
